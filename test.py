f32c7193a6eb01313b37e6d0303b1cff2358ce86
plutonh <jonghyen51@gmail.com>
2022-11-13 23:23:54 +0900

    test

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import serial
+import re
+from git.repo import Repo
 
 port = '/dev/ttyACM0'
 brate = 9600 #boudrate
 cmd = 'temp'
+repo = Repo('/home/plutonh')
 
 seri = serial.Serial(port, baudrate = brate, timeout = None)
 print(seri.name)
@@ -10,11 +13,18 @@ print(seri.name)
 seri.write(cmd.encode())
 
 a = 1
-cnt = 100
 
 while a:
-    cnt = cnt - 1
     if seri.in_waiting != 0 :
+        text = open('/home/plutonh/index_origin.html', 'r')
+        text = text.read()
         content = seri.readline()
         print(content[:-2].decode())
-
+        text = text.replace('data', content[:-2].decode())
+        with open('/home/plutonh/index.html', 'w') as f:
+         f.write(text)
+         f.close() 
+        repo.index.add(['index.html')
+        repo.index.commit('commit from python')
+        origin = repo.remotes[0]
+        origin.push()

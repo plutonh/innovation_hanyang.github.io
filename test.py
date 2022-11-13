d7e27c63b2b97dce8e92867157830405a312178d
plutonh <jonghyen51@gmail.com>
2022-11-14 01:17:22 +0900

    nam

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
+from git import Repo
 import serial
 import re
-from git.repo import Repo
 
 port = '/dev/ttyACM0'
 brate = 9600 #boudrate
@@ -13,9 +13,11 @@ print(seri.name)
 seri.write(cmd.encode())
 
 a = 1
+can = 0
 
 while a:
     if seri.in_waiting != 0 :
+        can = can + 1
         text = open('/home/plutonh/index_origin.html', 'r')
         text = text.read()
         content = seri.readline()
@@ -23,8 +25,13 @@ while a:
         text = text.replace('data', content[:-2].decode())
         with open('/home/plutonh/index.html', 'w') as f:
          f.write(text)
-         f.close() 
-        repo.index.add(['index.html')
-        repo.index.commit('commit from python')
-        origin = repo.remotes[0]
-        origin.push()
+         f.close()
+        if can == 10:
+         origin = repo.remote(name = 'origin')
+         origin.pull()
+         repo.index.add(['index.html'])
+         repo.index.commit('commit from python')
+         origin = repo.remote(name = 'origin')
+         origin.push()
+         #repo.git.push(force=True)
+         can = 0

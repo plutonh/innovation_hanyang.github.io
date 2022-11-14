c50074309850b4a15244c7737f54bf1cb46d32b4
plutonh <jonghyen51@gmail.com>
2022-11-14 23:07:35 +0900

    test

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from git import Repo
+import git
 import serial
 import re
 
 port = '/dev/ttyACM0'
 brate = 9600 #boudrate
 cmd = 'temp'
-repo = Repo('/home/plutonh/.git')
+repo = Repo.init('/home/plutonh')
+#git.Git('/home/plutonh').clone("git@github.com:plutonh/innovation.github.io.git")
+#repo.git.checkout('master')
+#repo = git.Repo('/home/plutonh')
+#repo.remote().fetch()
 
 seri = serial.Serial(port, baudrate = brate, timeout = None)
 print(seri.name)
@@ -14,6 +19,10 @@ seri.write(cmd.encode())
 
 a = 1
 can = 0
+
+#class ProgressPrinter(git.RemoteProgress):
+# def line_dropped(self, line):
+#  print("line dropped : " + str(line))
 
 while a:
     if seri.in_waiting != 0 :
@@ -32,6 +41,6 @@ while a:
          repo.index.add(['index.html'])
          repo.index.commit('commit from python')
          origin = repo.remote(name = 'origin')
-         #origin.push()
-         repo.git.push("--set-upstream", "origin", "master")
+         origin.push()
+         #repo.git.push("--set-upstream", "origin", "master")
          can = 0

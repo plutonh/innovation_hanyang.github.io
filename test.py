9b1ce0d358a1850d540623cf7ec82d7a2b8c72ea
plutonh <jonghyen51@gmail.com>
2022-11-18 01:04:20 +0900

    2022-11-18 01:04:20

--- test.py
+++ test.py
@@ -11,26 +11,29 @@ cmd = 'temp'
 repo = Repo.init('/home/plutonh')
 
 seri = serial.Serial(port, baudrate = brate, timeout = None)
-print(seri.name)
-
 seri.write(cmd.encode())
+#print(seri.name)
 
 a = 1
 can = 0
 
 while a:
     if seri.in_waiting != 0 :
-        can = can + 1
+        
+        content = seri.readline()
         text = open('/home/plutonh/index_origin.html', 'r')
         text = text.read()
-        content = seri.readline()
-        print(content[:-2].decode(), '\n', can)
         changed_time = time.strftime('%Y-%m-%d %H:%M:%S ', time.localtime(time.time()))
         text = text.replace('data', content[:-2].decode())
         text = text.replace('time', changed_time)
+
+        can = can + 1
+        #print(content[:-2].decode(), '\n', can)
+
         with open('/home/plutonh/index.html', 'w') as f:
          f.write(text)
          f.close()
+
         if can == 10:
          repo.index.add(['index.html'])
          repo.index.add(['test.py'])
